5a4b21a9cb6dbec0538bba4e0c807b3d583447ef
Alex Miquéias <alexkrypto.ti@gmail.com>
2024-05-16 16:28:43 -0300

    Ultimos ajustes para retorno da url encurtada.

--- src/main/java/com/tds/encurtadorurl/controller/UrlController.java
+++ src/main/java/com/tds/encurtadorurl/controller/UrlController.java
@@ -1,8 +1,11 @@
 package com.tds.encurtadorurl.controller;
 
 import com.tds.encurtadorurl.controller.dto.ShortenUrlRequest;
+import com.tds.encurtadorurl.controller.dto.ShortenUrlResponse;
 import com.tds.encurtadorurl.entities.UrlEntity;
 import com.tds.encurtadorurl.repository.UrlRepository;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,20 +23,21 @@ public class UrlController {
         this.urlRepository = urlRepository;
     }
 
-    @PostMapping(value="/shorten-url")
-    public ResponseEntity<Void> shortenUrl(@RequestBody ShortenUrlRequest request){
+    @PostMapping(value = "/shorten-url")
+    public ResponseEntity<ShortenUrlResponse> shortenUrl(@RequestBody ShortenUrlRequest request,
+                                           HttpServletRequest servletRequest) {
 
         String id;
-        do{
-            id = RandomStringUtils.randomAlphanumeric(5,10);
+        do {
+            id = RandomStringUtils.randomAlphanumeric(5, 10);
 
-        }while(urlRepository.existsById(id));
+        } while (urlRepository.existsById(id));
 
         urlRepository.save(new UrlEntity(id, request.url(), LocalDateTime.now().plusMinutes(1)));
 
+        var redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url", id);
 
 
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
     }
 }
---
+++ src/main/java/com/tds/encurtadorurl/controller/dto/ShortenUrlResponse.java
@@ -0,0 +1,4 @@
+package com.tds.encurtadorurl.controller.dto;
+
+public record ShortenUrlResponse(String url) {
+}
